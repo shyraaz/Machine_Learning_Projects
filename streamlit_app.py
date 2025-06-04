8fee62c53164717055ba5248455e6acb9802384b
shyraaz <67177864+shyraaz@users.noreply.github.com>
2025-06-04 20:18:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ with st.expander('Data'):
   st.write('this is pinguin classification Task')
   df = pd.read_csv('https://raw.githubusercontent.com/rafaelcavasani/EAD-Penguins-Dataset/refs/heads/master/penguins_size.csv')
   df
-  df['Adelie'].values_count().plot(kind='bar')
+  df['species'].values_count().plot(kind='bar')
