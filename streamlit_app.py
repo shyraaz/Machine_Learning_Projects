998927b39d1ab85503e8f5e22263d016c83a0c19
shyraaz <67177864+shyraaz@users.noreply.github.com>
2025-06-04 19:47:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
-
+import pandas as pd
 st.title('🎈 Machine Learning Projects')
 st.info('Data Scientist')
 
-st.write('Hello world!')
+st.write('this is pinguin classification Task')
+df = pd.read_csv('https://raw.githubusercontent.com/rafaelcavasani/EAD-Penguins-Dataset/refs/heads/master/penguins_size.csv')
+df.head()
