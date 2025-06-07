0c6320ea5f3ca330ed2de29e2d210723f9373daf
shyraaz <67177864+shyraaz@users.noreply.github.com>
2025-06-07 15:26:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 st.title('🎈 Machine Learning Projects')
 st.info('Data Scientist') 
+df = pd.read_csv('https://raw.githubusercontent.com/rafaelcavasani/EAD-Penguins-Dataset/refs/heads/master/penguins_size.csv')
+
 with st.expander('Data'):
   st.write('this is pinguin classification Task')
   df = pd.read_csv('https://raw.githubusercontent.com/rafaelcavasani/EAD-Penguins-Dataset/refs/heads/master/penguins_size.csv')
   df
-  df['species'].value_counts().plot(kind='bar')
+df['species'].value_counts().plot(kind='bar')
